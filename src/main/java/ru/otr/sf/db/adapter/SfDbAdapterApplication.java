ce28bbd89e87b40aba04b2c451be0ea9107834e4
Nastya <anastasya.kiryuxina@yandex.ru>
2023-01-18 10:18:10 +0300

    Изменение версии Spring Boot на 2.7.7

--- src/main/java/ru/otr/sf/db/adapter/SfDbAdapterApplication.java
+++ src/main/java/ru/otr/sf/db/adapter/SfDbAdapterApplication.java
@@ -2,7 +2,8 @@ package ru.otr.sf.db.adapter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+@EnableWebMvc
 @SpringBootApplication
 public class SfDbAdapterApplication {
 
